5558a5a1a7c28bd4eb9549c2427fabe85a415dea
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-01-09 10:28:31 -0600

    Initial Commit

---
+++ WeekOneTimedAssessment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeekOneTimedAssessment
+//
+//  Created by Kyle Magnesen on 1/9/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
